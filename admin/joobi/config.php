f1dd5f6668d2981351292b03c9b38a0b529a6e9e
joobi <joobi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 15:09:09 +0000

    First commit of jCatalog to WP SVN repository
    git-svn-id: https://plugins.svn.wordpress.org/jcatalog/trunk@1198082 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/joobi/config.php
@@ -0,0 +1,20 @@
+<?php
+/** @copyright Copyright (c) 2007-2015 Joobi Limited. All rights reserved.
+* @link joobi.co
+* @license GNU GPLv3 */
+
+//defined('JOOBI_SECURE') or define( 'JOOBI_SECURE', true );
+
+class Joobi_Config{
+public $model = array(
+'tablename'=>'#__model_node'
+);
+public $table = array(
+'tablename'=>'#__dataset_tables'
+);
+public $db = array(
+'tablename'=>'#__dataset_node'
+);
+public $multiDB = false;
+public $secret = 'JoobiDev';
+}//endclass
